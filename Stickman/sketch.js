bdf3ceb607cbd05fd905c7a02f1b0e9a8113685a
DmalleWors <72189014+DmalleWors@users.noreply.github.com>
2020-11-12 10:22:09 +0100

    Add files via upload

---
+++ Stickman/sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(400);
+ 
+  ellipse(200,50,60);
+  line(200,80,200,200)
+  line(100,80,200,100)
+  line(300,80,200,100)
+  line(100,300,200,200)
+  line(300,300,200,200)
+  ellipse(190,40,10);
+  ellipse(210,40,10);
+   arc (200,30,60,12,50,60);
+   arc (200,55,30,50,50,60);
+ 
+  
+}
+
